2b4cc4ba2a965c77b1df2437f0b3012a98dc07d0
juancarlosgdm <juancarlosgdm@gmail.com>
2020-03-05 11:17:38 +0100

    Pause implemented

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour {
+
+    [Tooltip("Referencia al texto del botón")]
+    [SerializeField]
+    private Text buttonText;
+
+    #region Unity Messages
+
+    private void Start() {
+        // Se asegura que la pausa está desactivada al inicio
+        Time.timeScale = 1;
+    }
+
+    #endregion
+
+    #region Public Messages
+
+    public void ButtonClicked() {
+        if (Time.timeScale == 1) {
+            PauseGame();
+        } else {
+            ResumeGame();
+        }
+    }
+
+    #endregion
+
+    private void PauseGame() {
+        // Pausa del juego
+        Time.timeScale = 0;
+        buttonText.text = "Reanudar";
+    }
+
+    private void ResumeGame() {
+        // Reanudación del juego
+        Time.timeScale = 1;
+        buttonText.text = "Pausa";
+    }
+}
